baa1f69eac51e7ca65c16d5edd0a1233359483b2
Drugashev Maksim <enziofael@gmail.com>
2026-07-13 15:41:51 +0300

    +textMessage.go

---
+++ frontend-bot/internal/actions/textMessage.go
@@ -0,0 +1,23 @@
+// ./frontend-bot/internal/actions/testMessage.go
+
+package actions
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+// Send
+
+func SendText(bot *tgbotapi.BotAPI, config tgbotapi.MessageConfig) {
+	if _, err := bot.Send(config); err != nil {
+		HandleSendError(bot, config)
+	}
+}
+
+// Edit
+
+func EditText(bot *tgbotapi.BotAPI, config tgbotapi.EditMessageTextConfig) {
+	if _, err := bot.Request(config); err != nil {
+		HandleRequestError(bot, config)
+	}
+}
